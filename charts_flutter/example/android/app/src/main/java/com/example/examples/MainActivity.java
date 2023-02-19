0e0c1cc410b1349a16358913cfaa0ea21857acc4
Alexander Flammer <alex.flammer.dev@outlook.com>
2023-02-19 15:51:57 +0100

    update example app to android embedding v2

--- charts_flutter/example/android/app/src/main/java/com/example/examples/MainActivity.java
+++ charts_flutter/example/android/app/src/main/java/com/example/examples/MainActivity.java
@@ -2,16 +2,12 @@ package com.example.examples;
 
 import android.os.Bundle;
 
-import io.flutter.app.FlutterActivity;
+import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
 /**
  * FlutterActivity
  */
 public class MainActivity extends FlutterActivity {
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
-  }
+
 }
